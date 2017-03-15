ecf85015ba13bf00843086b269d091d635eb7729
Evan Albert <evan@cyberdust.com>
2017-03-15 16:49:35 -0700

    refactor app to work with local storage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -6,53 +6,18 @@ import Search from 'Search';
 
 const uuid = require('node-uuid');
 const moment = require('moment');
-const TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-  getInitialState: function () {
-    return {
-      showCompleted: false,
-      searchText: '',
-      todos: TodoAPI.getTodos()
-    }
-  },
-  componentDidUpdate: function () {
-    TodoAPI.setTodos(this.state.todos);
-  },
-  handleSearch: function (showCompleted, searchText) {
-    this.setState({
-      showCompleted: showCompleted,
-      searchText: searchText.toLowerCase()
-    })
-  },
-  handleAddTodo: function (text) {
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        {
-          id: uuid(),
-          text: text,
-          completed: false,
-          createdAt: moment().unix(),
-          completedAt: undefined
-        }
-      ]
-    })
-  },
   render: function () {
-    var {todos, showCompleted, searchText} = this.state;
-
-    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
-
     return (
       <div>
         <h3 className="page-title">Todo App</h3>
         <div className="row">
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
-              <Search onSearch={this.handleSearch}/>
+              <Search/>
               <TodoList/>
-              <AddTodo addTodo={this.handleAddTodo}/>
+              <AddTodo/>
             </div>
           </div>
         </div>
